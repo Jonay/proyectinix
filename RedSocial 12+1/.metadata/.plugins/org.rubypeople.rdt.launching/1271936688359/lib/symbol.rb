cbefdc1532c5e7996f626376fa221d84a1ac16e9
Jonay <jsantana.gcc@gmail.com>
2010-11-10 09:39:44 +0000

    Con funcionamiento de los mapas al crear un evento.

---
+++ RedSocial 12+1/.metadata/.plugins/org.rubypeople.rdt.launching/1271936688359/lib/symbol.rb
@@ -0,0 +1,36 @@
+class Symbol < Object
+
+  def self.all_symbols
+  end
+
+  def to_yaml_node(arg0)
+  end
+
+  def to_s
+  end
+
+  def id2name
+  end
+
+  def ===(arg0)
+  end
+
+  def hash
+  end
+
+  def to_int
+  end
+
+  def to_sym
+  end
+
+  def inspect
+  end
+
+  def to_i
+  end
+
+  def taguri
+  end
+
+end
