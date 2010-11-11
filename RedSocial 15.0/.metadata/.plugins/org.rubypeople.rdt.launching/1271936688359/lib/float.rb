bf183e61c365fb85b18928b823b8a2d186b96787
Jonay <jsantana.gcc@gmail.com>
2010-11-11 12:42:54 +0000

    Con fotos de portada de los albumes en los grupos.

---
+++ RedSocial 15.0/.metadata/.plugins/org.rubypeople.rdt.launching/1271936688359/lib/float.rb
@@ -0,0 +1,113 @@
+class Float < Numeric
+  include Precision
+  include Comparable
+
+  def self.yaml_tag_subclasses?
+  end
+
+  def self.induced_from(arg0)
+  end
+
+  def floor
+  end
+
+  def infinite?
+  end
+
+  def nan?
+  end
+
+  def modulo(arg0)
+  end
+
+  def ==(arg0)
+  end
+
+  def /(arg0)
+  end
+
+  def +(arg0)
+  end
+
+  def finite?
+  end
+
+  def <=>(arg0)
+  end
+
+  def to_i
+  end
+
+  def taguri
+  end
+
+  def eql?(arg0)
+  end
+
+  def to_f
+  end
+
+  def divmod(arg0)
+  end
+
+  def round
+  end
+
+  def <(arg0)
+  end
+
+  def coerce(arg0)
+  end
+
+  def *(arg0)
+  end
+
+  def **(arg0)
+  end
+
+  def >(arg0)
+  end
+
+  def to_yaml_node(arg0)
+  end
+
+  def to_s
+  end
+
+  def %(arg0)
+  end
+
+  def -@
+  end
+
+  def >=(arg0)
+  end
+
+  def ceil
+  end
+
+  def taguri=(arg0, arg1, *rest)
+  end
+
+  def hash
+  end
+
+  def to_int
+  end
+
+  def abs
+  end
+
+  def truncate
+  end
+
+  def <=(arg0)
+  end
+
+  def zero?
+  end
+
+  def -(arg0)
+  end
+
+end
---
+++ RedSocial 15.0/.metadata/.plugins/org.rubypeople.rdt.launching/1272368822997/lib/exception.rb
@@ -0,0 +1,39 @@
+=begin
+------------------------------------------------------- Class: Exception
+     (no description...)
+------------------------------------------------------------------------
+
+=end
+class Exception < Object
+
+  def self.exception(arg0, arg1, *rest)
+  end
+
+  def to_yaml_node(arg0)
+  end
+
+  def to_s
+  end
+
+  def message
+  end
+
+  def set_backtrace(arg0)
+  end
+
+  def backtrace
+  end
+
+  def exception(arg0, arg1, *rest)
+  end
+
+  def to_str
+  end
+
+  def inspect
+  end
+
+  def taguri
+  end
+
+end
