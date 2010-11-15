6591a190a3f5aa0ff9988b69563c0ddd62416578
Jonay <jsantana.gcc@gmail.com>
2010-11-15 12:30:57 +0000

    Con funcionamiento perfecto de los moderadores de los grupos.

---
+++ RedSocial 18.0/.metadata/.plugins/org.rubypeople.rdt.launching/1272368822997/lib/regexp.rb
@@ -0,0 +1,75 @@
+class Regexp < Object
+
+  def self.escape(arg0, arg1, *rest)
+  end
+
+  def self.quote(arg0, arg1, *rest)
+  end
+
+  def self.last_match(arg0, arg1, *rest)
+  end
+
+  def self.compile(arg0, arg1, *rest)
+  end
+
+  def self.new(arg0, arg1, *rest)
+  end
+
+  def self.union(arg0, arg1, *rest)
+  end
+
+  def self.yaml_tag_subclasses?
+  end
+
+  def match
+  end
+
+  def ~
+  end
+
+  def casefold?
+  end
+
+  def kcode
+  end
+
+  def to_yaml_node
+  end
+
+  def to_s
+  end
+
+  def options
+  end
+
+  def taguri=
+  end
+
+  def ==
+  end
+
+  def =~
+  end
+
+  def ===
+  end
+
+  def hash
+  end
+
+  def initialize_copy
+  end
+
+  def inspect
+  end
+
+  def source
+  end
+
+  def taguri
+  end
+
+  def eql?
+  end
+
+end
---
+++ RedSocial 18.0/.metadata/.plugins/org.rubypeople.rdt.launching/1272368822997/lib/singleton.rb
@@ -0,0 +1,12 @@
+module Singleton
+
+  def self.__init__(arg0)
+  end
+
+  def dup
+  end
+
+  def clone
+  end
+
+end
